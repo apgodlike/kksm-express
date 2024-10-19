d2005f076ab0d0d712d9fe86475b970ba8ceabbd
Godlike <apgodlike@protonmail.com>
2024-10-19 14:57:43 +0530

    mobile OTP Schema

--- src/schemas/mobileOtpSchema.ts
+++ src/schemas/mobileOtpSchema.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 export const mobileOtpSchema = z.object({
-  mobile_number: z.number(),
+  mobile_number: z
+    .number()
+    .refine((val) => val >= 1000000000 && val <= 9999999999, {
+      message: "Mobile number must be a 10-digit number",
+    }),
 });
 
 export type MobileOtpSchema = z.infer<typeof mobileOtpSchema>;
