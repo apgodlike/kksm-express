b86491f1eb15630701854b435fa6ae31751992fd
Godlike <apgodlike@protonmail.com>
2025-03-31 20:39:39 +0530

    CORS error fix

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,18 @@ app.use(
     exposedHeaders: ["Set-Cookie"],
   })
 );
+
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204); // No Content
+});
+
 app.use(cookieParser());
 app.use(express.json());
 
