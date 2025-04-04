92d08418f8b6e823844b19aefcf67dedac24f727
Godlike <apgodlike@protonmail.com>
2025-04-04 11:46:33 +0530

    remove commented lines

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -49,48 +49,6 @@ export const saveProfile = async (req: Request, res: Response) => {
     const now = new Date();
     //
 
-    // const payload = {
-    //   // @ts-ignore
-    //   userId: req.user.userId,
-    //   isProfileCompleted: true,
-    //   isActive:
-    //     !userRecord.expires_at || userRecord.expires_at < now ? false : true,
-    // };
-
-    // const token = generateAccessToken(payload);
-    // const refreshToken = generateRefreshToken(payload);
-
-    // await prisma.refreshToken.create({
-    //   data: {
-    //     token: refreshToken,
-    //     user_id: payload.userId,
-    //     expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    //   },
-    // });
-
-    // await prisma.refreshToken.upsert({
-    //   where: { user_id: payload.userId },
-    //   update: {
-    //     token: refreshToken,
-    //     expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    //   },
-    //   create: {
-    //     token: refreshToken,
-    //     user_id: payload.userId,
-    //     expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    //   },
-    // });
-
-    // res.cookie("refreshToken", refreshToken, {
-    //   httpOnly: true,
-    //   secure: process.env.NODE_ENV === "production",
-    //   sameSite: "strict",
-    //   maxAge: 7 * 24 * 60 * 60 * 1000,
-    //   path: "/",
-    //   // domain: ".kovaikongumatrimony.com",
-    //   domain: getCookieDomain(),
-    //   // domain: "192.168.29.126",
-    // });
     await setUserClaims(userRecord.id, {
       userId: userRecord.id,
       isRegistered: true,
@@ -98,18 +56,6 @@ export const saveProfile = async (req: Request, res: Response) => {
     });
 
     res.status(200).json({ message: "Created" });
-    // res.status(200).json({ token, refreshToken });
-
-    /////
-    /* const token = generateAccessToken({
-      // @ts-ignore
-      userId: req.user.userId,
-      isProfileCompleted: true,
-      isActive:
-        !userRecord.expires_at || userRecord.expires_at < now ? false : true,
-    }); */
-
-    // return res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ error });
   }
